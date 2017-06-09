45e21df5c9df2baef09e61c3bb55b4cfe4933834
bocharovf <bocharovf@gmail.com>
2017-06-10 00:47:41 +0300

    Progress component fixed

--- src/app/result/progress.component.ts
+++ src/app/result/progress.component.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-    selector: 'app-progress',
+    selector: 'result-progress',
     templateUrl: './progress.component.html',
     styleUrls: ['progress.component.scss']
 })
 export class ProgressComponent {
-
+  @Input() displayText: string;
 }
