fde4f6f74ef529eb614a15c727140f0de5048e12
bocharovf <bocharovf@gmail.com>
2017-06-11 19:26:47 +0300

    Convert areas tree to flat array

--- src/app/filter/FilterItem.ts
+++ src/app/filter/FilterItem.ts
@@ -1,6 +1,20 @@
-
+/**
+ * Represents filter collection element
+ */
 export class FilterItem{
-  constructor(public name: string, public id: any, public group?: string) {
+
+  /**
+   * Create new FilterItem
+   * @param name display name to show
+   * @param id unique identifier
+   * @param group display name of element group
+   * @param level nested level of element
+   */
+  constructor(
+    public name: string,
+    public id: any,
+    public group?: string,
+    public level?: number) {
   }
 
   public toString = () : string => {
--- src/app/search/dictionary.service.ts
+++ src/app/search/dictionary.service.ts
@@ -22,13 +22,45 @@ export class DictionaryService {
   constructor () {
     this.areas = Observable
       .fromPromise(this.api.getAreas())
-      .map(areas => areas.map(area => new FilterItem(area.name, area.id, area.name)));
+      .map(areas => DictionaryService.areasTree2FlatArray(areas));
     this.areas.subscribe();
 
     this.experiences = Observable
       .fromPromise(this.api.getExperincies())
       .map(experiences => experiences.map(exp => new FilterItem(exp.name, exp.id, null)));
     this.experiences.subscribe();
+  }
+
+  /**
+   * Convert hierarchical tree of areas to flat array of filter items
+   * @param areasTree areas tree
+   * @returns {AreaItem[]} flat array of areas
+   */
+  private static areasTree2FlatArray(areasTree: AreaItem[]): FilterItem[]{
+    let stack: {area: AreaItem, level: number, group: string}[];
+    stack = areasTree.map(area => ({ area: area, level: 0, group: 'Страна'}));
+    let flatArray: FilterItem[] = [];
+
+    while(stack.length !== 0) {
+      let item = stack.pop();
+      let area = item.area;
+
+      let filterItem = new FilterItem(area.name, area.id, item.group, item.level);
+      flatArray.push(filterItem);
+
+      if(area.areas && area.areas.length > 0) {
+        let newItems = area.areas.map(a =>
+          ({
+            area: a,
+            level: item.level + 1,
+            group: area.name
+          })
+        );
+        stack.push(...newItems);
+      }
+    }
+
+    return flatArray;
   }
 
 }
