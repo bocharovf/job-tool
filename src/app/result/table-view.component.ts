74daeee4ae58c8e99e18b0978e163a856ad09116
bocharovf <bocharovf@gmail.com>
2017-06-17 17:01:32 +0300

    Add color anf filter values to TableView component

--- src/app/result/table-view.component.ts
+++ src/app/result/table-view.component.ts
@@ -5,22 +5,31 @@ import {SearchModel} from "../search/SearchModel";
     selector: 'result-table-view',
     template: `
 <div class="table-responsive table-container">
-  <table class="table-striped">
+  <table class="table table-striped table-hover table-bordered">
     <tr>
       <th>№</th>
       <th>Запрос</th>
+      <th>Город</th>
+      <th>Опыт</th>
       <th>Кол-во вакансий</th>
       <th>Средняя з.п.</th>
     </tr>
-    <tr *ngFor="let result of results">
-      <td>1</td>
-      <td>
-        {{result.query}} 
-        {{result.area ? result.area.name : ''}} 
-        {{result.experience ? result.experience.name : ''}}
+    <tr *ngFor="let result of results; index as i">
+      <td>{{i+1}}</td>
+      <td class="strong-text">
+        <div class="circle" [style.background-color]="result.color"></div>
+        {{result.query}}
       </td>
+      <td [class.default-text]="!result.area">
+        {{result.area ? result.area.name : 'любой'}}
+      </td>
+      <td [class.default-text]="!result.experience">
+        {{result.experience ? result.experience.name : 'любой'}}
+        </td>
       <td>{{result.stat.amount}}</td>
-      <td>{{result.stat.avgSalary.toFixed(0)}}</td>            
+      <td class="strong-text">
+        {{result.stat.avgSalary.toFixed(0)}} руб.
+      </td>            
     </tr>  
   </table>
 </div>
