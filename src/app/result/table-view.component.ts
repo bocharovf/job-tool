fad70c3434a0a1919a59bddae887f0217f19af73
bocharovf <bocharovf@gmail.com>
2017-06-18 19:25:23 +0300

    Fix table view layout

--- src/app/result/table-view.component.ts
+++ src/app/result/table-view.component.ts
@@ -6,31 +6,35 @@ import {SearchModel} from "../search/SearchModel";
     template: `
 <div class="table-responsive table-container">
   <table class="table table-striped table-hover table-bordered">
-    <tr>
-      <th>№</th>
-      <th>Запрос</th>
-      <th>Город</th>
-      <th>Опыт</th>
-      <th>Кол-во вакансий</th>
-      <th>Средняя з.п.</th>
-    </tr>
-    <tr *ngFor="let result of results; index as i">
-      <td>{{i+1}}</td>
-      <td class="strong-text">
-        <div class="circle" [style.background-color]="result.color"></div>
-        {{result.query}}
-      </td>
-      <td [class.default-text]="!result.area">
-        {{result.area ? result.area.name : 'любой'}}
-      </td>
-      <td [class.default-text]="!result.experience">
-        {{result.experience ? result.experience.name : 'любой'}}
+    <thead>
+      <tr>
+        <th>№</th>
+        <th>Запрос</th>
+        <th>Город</th>
+        <th>Опыт</th>
+        <th>Кол-во вакансий</th>
+        <th>Средняя з.п.</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr *ngFor="let result of results; index as i">
+        <td>{{i+1}}</td>
+        <td class="strong-text">
+          <div class="circle" [style.background-color]="result.color"></div>
+          {{result.query}}
         </td>
-      <td>{{result.stat.amount}}</td>
-      <td class="strong-text">
-        {{result.stat.avgSalary.toFixed(0)}} руб.
-      </td>            
-    </tr>  
+        <td [class.default-text]="!result.area">
+          {{result.area ? result.area.name : 'любой'}}
+        </td>
+        <td [class.default-text]="!result.experience">
+          {{result.experience ? result.experience.name : 'любой'}}
+          </td>
+        <td>{{result.stat.amount}}</td>
+        <td class="strong-text">
+          {{result.stat.avgSalary.toFixed(0)}} руб.
+        </td>            
+      </tr>  
+    </tbody>  
   </table>
 </div>
 `,
